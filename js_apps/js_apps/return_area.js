d68ef293cbadf4109591fcaa1b86499c9c70eedd
Fatima Muhammed <faaman2020@gmail.com>
2025-08-02 20:38:00 +0100

    now have the correct files

--- js_apps/js_apps/return_area.js
+++
@@ -1,9 +0,0 @@
-var area = (function() {
-    var width = 3;
-    var height = 2;
-    return width * height;
-} ());
-function updateArea() {
-    document.getElementById('find_area').textContent = area;
-}
-updateArea();
