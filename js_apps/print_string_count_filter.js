45037ad58f7c0c3ee81522254824d8f04cd28ebf
Fatima Muhammed <faaman2020@gmail.com>
2025-08-04 11:29:46 +0100

    updates

--- js_apps/print_string_count_filter.js
+++ js_apps/print_string_count_filter.js
@@ -3,7 +3,7 @@ var mycount_filter = (function() {
     const string_list = ['abc', 'def', 'abc', 'ghi'];    //can also test this for []
     const string_val = 'abc';    //can also test this for null
 
-    return result = string_list.filter(str => str === string_val).length;
+    return string_list.filter(str => str === string_val).length;
 
 } ());
 
